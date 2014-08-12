551fb1b3f0788fb63b4867ee698e50e2231d4901
Vinay Mayar <vinaymayar@gmail.com>
2014-08-12 14:20:11 -0400

    Add packages

--- install.sh
+++ install.sh
@@ -32,11 +32,12 @@ cp -rf ./vinrc $vimrc
 
 githubdirs=("nerdtree" "ctrlp.vim" "vim-easymotion" "vim-multiple-cursors"\
     "vim-colors-solarized" "vim-fugitive" "vim-scala" "vim-coffee-script" "ack.vim"\
-    "vim-go" "html5" "play2vim")
+    "vim-go" "html5" "play2vim" "vim-less" "vim-css3-syntax")
 repos=("scrooloose/nerdtree.git" "kien/ctrlp.vim" "Lokaltog/vim-easymotion"\
     "terryma/vim-multiple-cursors.git" "altercation/vim-colors-solarized.git"\
     "tpope/vim-fugitive" "derekwyatt/vim-scala.git" "kchmck/vim-coffee-script"\
-    "mileszs/ack.vim" "fatih/vim-go.git" "othree/html5.vim" "gre/play2vim.git")
+    "mileszs/ack.vim" "fatih/vim-go.git" "othree/html5.vim" "gre/play2vim.git"\
+    "groenewege/vim-less" "hail2u/vim-css3-syntax")
 
 for idx in ${!githubdirs[*]}
 do
